861755a719fd18684195a8acff64450e5ce9cc2b
Maksym <malsimushkka@gmail.com>
2018-11-10 01:50:00 +0300

    add excel

--- config/app.php
+++ config/app.php
@@ -160,6 +160,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Jenky\LaravelPlupload\PluploadServiceProvider::class,
+        Maatwebsite\Excel\ExcelServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -223,6 +224,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Plupload' => Jenky\LaravelPlupload\Facades\Plupload::class,
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
 
     ],
 

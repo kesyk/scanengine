735deb624d4720542f386f0ec83116a37944c411
Kiiashkin Maksym <malsimushkka@gmail.com>
2018-11-28 10:07:03 -0500

    transfer upload logic from controller to service

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -2,11 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\AmazonSearch;
-use App\Jobs\SearchProcessor;
-use Box\Spout\Common\Type;
-use Box\Spout\Reader\ReaderFactory;
-use Illuminate\Support\Facades\Storage;
+use App\Models\UserMatch;
+use App\Services\UploadService;
+use Exception;
 use Illuminate\Http\Request;
 
 class UploadController extends Controller
@@ -16,95 +14,32 @@ class UploadController extends Controller
         return view('main.upload');
     }
 
-    public function upload()
+    public function uploadFileToDisk()
     {
-        $uploadedFile = $this->uploadFileToDisk();
-        if ($uploadedFile != '')
+        try{
+            $uploadService = new UploadService();
+
+            return $uploadService->uploadFileToDisk();
+        }
+        catch (Exception $ex)
         {
-            $reader = ReaderFactory::create(Type::XLSX);
-            $reader->open($uploadedFile);
-            foreach ($reader->getSheetIterator() as $sheet) {
-                foreach ($sheet->getRowIterator() as $row) {
-                    $headerColumns = $row;
-                    break;
-                }
-                break;
-            }
-            $reader->close();
-
-            return response($headerColumns);
+            return response($ex->getMessage(), 400);
         }
     }
 
-    public function createSearch(Request $request)
+    public function uploadFileToDb(Request $request)
     {
-        $userMatch = (object)$request->productData;
-        $fileName = (object)$request->fileName;
+        try{
+            $userMatch = new UserMatch((object)$request->productData);
+            $fileName = ((object)$request->fileName)->scalar;
 
-        #region Validation
+            $uploadService = new UploadService();
 
-        if($userMatch == null)
-            response(array("type" => "error", "message" => "Columns are not filled"));
-
-        if($userMatch->upc == 'none' &&
-            $userMatch->asin == 'none' &&
-            $userMatch->title == 'none')
-            response(array("type" => "error", "message" => "Main fields are not filled. Please fill ASIN, UPS or Title column in mathing section"));
-
-        #endregion
-
-        $search = new AmazonSearch();
-        $search->originalname = $fileName;
-        $search->hashedname = md5($search->id.$fileName->name);
-    }
-
-    private function uploadFileToDisk()
-    {
-        if (empty($_FILES) || $_FILES['file']['error']) {
-            die('{"OK": 0, "info": "Failed to move uploaded file."}');
+            $uploadService->uploadFileToDb($userMatch, $fileName);
         }
-
-        $chunk = isset($_REQUEST["chunk"]) ? intval($_REQUEST["chunk"]) : 0;
-        $chunks = isset($_REQUEST["chunks"]) ? intval($_REQUEST["chunks"]) : 0;
-
-        $fileName = isset($_REQUEST["name"]) ? $_REQUEST["name"] : $_FILES["file"]["name"];
-        $filePath = Storage::disk('local')->getAdapter()->getPathPrefix().'/'.$fileName;
-
-        // Open temp file
-        $out = @fopen("{$filePath}.part", $chunk == 0 ? "wb" : "ab");
-        if ($out) {
-            // Read binary input stream and append it to temp file
-            $in = @fopen($_FILES['file']['tmp_name'], "rb");
-
-            if ($in) {
-                while ($buff = fread($in, 4096))
-                    fwrite($out, $buff);
-            } else
-                die('{"OK": 0, "info": "Failed to open input stream."}');
-
-            @fclose($in);
-            @fclose($out);
-
-            @unlink($_FILES['file']['tmp_name']);
-        } else
-            die('{"OK": 0, "info": "Failed to open output stream."}');
-
-
-// Check if file has been uploaded
-        if (!$chunks || $chunk == $chunks - 1) {
-            // Strip the temp .part suffix off
-            rename("{$filePath}.part", $filePath);
-
-            return $filePath;
+        catch (Exception $ex)
+        {
+            return response($ex->getMessage(), 400);
         }
-    }
-
-    public function start()
-    {
-
-        $bgWork = new SearchProcessor();
-
-        $this->dispatch($bgWork);
-
     }
 }

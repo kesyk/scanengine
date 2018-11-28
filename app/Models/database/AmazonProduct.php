6c3c780a746ca0ac8664560e33a4cd6825ae2fce
Kiiashkin Maksym <malsimushkka@gmail.com>
2018-11-28 10:09:20 -0500

    add model mapping of file columns data and user match

--- app/Models/database/AmazonProduct.php
+++ app/Models/database/AmazonProduct.php
@@ -2,37 +2,36 @@
 
 namespace App;
 
+use App\Models\MatchedResult;
 use Illuminate\Database\Eloquent\Model;
 
 class AmazonProduct extends Model
 {
     protected $table;
-    protected $fillable = [
-        'id',
-        'hashedname',
-        'upc',
-        'price',
-        'asin',
-        'salesrank',
-        'title',
-        'brand',
-        'atitle',
-        'packagequantity',
-        'aboxprice',
-        'newoffers',
-        'category',
-        'fbafee',
-        'reffee',
-        'varclosingfee',
-        'shippingcost',
-        'profit',
-        'roi',
-    ];
+
     public function __construct($tableName, array $attributes = [])
     {
         parent::__construct($attributes);
 
-        $this->table = $tableName;
+        $this->table = "upload_".$tableName;
+    }
+
+    public function setupProductByUserMatch(MatchedResult $matchedIndexes, $fileRow, $hashedName){
+        $this->hashedname = $hashedName;
+        if($matchedIndexes->titleIndex)
+            $this->title = $fileRow[$matchedIndexes->titleIndex];
+        if($matchedIndexes->priceIndex)
+            $this->price = $fileRow[$matchedIndexes->priceIndex];
+        if($matchedIndexes->asinIndex)
+            $this->asin = $fileRow[$matchedIndexes->asinIndex];
+        if($matchedIndexes->upcIndex)
+            $this->upc = $fileRow[$matchedIndexes->upcIndex];
+        if($matchedIndexes->quantityIndex)
+            $this->quantity = $fileRow[$matchedIndexes->quantityIndex];
+        if($matchedIndexes->urlIndex)
+            $this->url = $fileRow[$matchedIndexes->urlIndex];
+        if($matchedIndexes->imageIndex)
+            $this->image = $fileRow[$matchedIndexes->imageIndex];
     }
 
 }
